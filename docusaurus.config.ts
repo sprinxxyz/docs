c9bf0c78b9878444ab6c771ae605e356f47f0199
Lima 🍋‍🟩 <polychimpz@hotmail.com>
2025-07-26 22:34:07 +0700

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   url: 'https://docs.sprinx.xyz',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs',
+  baseUrl: '/',
 
   customFields: {
     freeTierAmount: process.env.FREE_TIER_AMOUNT || '$20',
